868cfd68958367b484964328c5e5e46ce9254213
lfillo-textiq <83605068+lfillo-textiq@users.noreply.github.com>
2021-09-16 13:35:07 -0700

    Update install_latest_bartender.sh

--- scripts/install_latest_bartender.sh
+++ scripts/install_latest_bartender.sh
@@ -2,4 +2,4 @@
 
 source /usr/local/etc/secrets.config
 export GITHUB_PAT=$GITHUB_PAT
-python3 /opt/textiq/config/scripts/bartender/install_latest_bartender.py
+python3 ./scripts/install_latest_bartender.py

dcd871ce59d8cde402659b707105af6cdef25498
lfillo-textiq <83605068+lfillo-textiq@users.noreply.github.com>
2021-11-09 13:45:07 -0800

    Update probot.js

--- probot.js
+++ probot.js
@@ -3,5 +3,5 @@ const { Probot } = require("probot");
 const probot = new Probot({
   privateKey: process.env.GITHUB_PRIVATE_KEY,
   appId: process.env.APP_ID,
-  secret process.env.WEBHOOK_SECRET,
+  secret: process.env.WEBHOOK_SECRET,
 })
